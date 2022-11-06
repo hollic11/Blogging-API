566f7b79e9d425073e0c0c48aa8d45b3c4afda3d
hollic11 <odetokunelijah8@gmail.com>
2022-11-06 12:06:47 +0100

    initial commit

---
+++ model/user.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+const Schema = mongoose.Schema
+const ObjectId = Schema.ObjectId
+
+const UserSchema = new mongoose.Schema({
+    first_name:{
+        type: String,
+         required: true,
+     },
+
+    last_name:{
+        type: String,
+         required: true,
+     },
+
+    email:{
+        type: String,
+         required: true,
+         unique:true
+     },
+
+     password:{
+        type: String,
+        required: true,
+        min:8,
+        max:15
+     },
+
+     user_type:  { 
+        type: String, 
+        required: true, 
+        enum: ['user', 'admin'], 
+        default: 'user' 
+    },
+timestamps: true
+});
+
+UserModel.pre(
+    'save', 
+    async function (next) {
+        const user = this;
+        const hash = await bcrypt.hash(this.password, 10)
+        
+        this.password = hash
+        next()
+    }
+)
+UserModel.methods.isValidPassword = async function(password) {
+    const user = this;
+    const compare = await bcrypt.compare(password, user.password);
+  
+    return compare;
+  }
+  
+module.exports = mongoose.model("User", UserSchema);
