e747ca64b76956450b0cb6c1f793536b301d05bb
Elijah Odetokun <105095532+hollic11@users.noreply.github.com>
2022-11-06 17:55:59 +0100

    Update blog_route.js

--- route/blog_route.js
+++ route/blog_route.js
@@ -3,8 +3,8 @@ const BlogController = require('../controllers/BlogController')
 const blogRouter = express.Router();
 
 blogRouter.post('/', BlogController.createBlog)
-blogRouter.get('/:blogId', BlogController.getBlog)
-blogRouter.get('/', BlogController.getBlogs)
+blogRouter.get('/:blogId', BlogController.getBlogById)
+blogRouter.get('/', BlogController.getAllBlogs)
 blogRouter.patch('/:id', BlogController.updateBlog)
 blogRouter.delete('/:id', BlogController.deleteBlog)
 
